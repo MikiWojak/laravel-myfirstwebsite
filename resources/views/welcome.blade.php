fb0b3f50273e85aed0ca3af56080277f668c8fe7
MikiWojak <mikizarowa8@gmail.com>
2021-02-27 11:39:19 +0100

    List of tasks in the main page
    - Send the list of task to the main page
    - Show the list of tasks
    - Done in a bit ugly way

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,12 @@
 
 @section('content')
   <h1>My First Website</h1>
+
+  <ul>
+
+    <?php foreach ($tasks as $task) : ?>
+      <li><?= $task; ?></li>
+    <?php endforeach; ?>
+
+  </ul>
 @endsection
